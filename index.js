9289750dd2baa58ea4be3cb75e3a7c614aed1d27
Onesun15 <sunnysharma15@hotmail.com>
2018-01-08 12:11:12 -0500

    use strict and modify spacing

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
+'use strict';
+
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const morgan = require('morgan');
-const {PORT, CLIENT_ORIGIN, DATABASE_URL} = require('./config');
-const {dbConnect} = require('./db-mongoose');
-const mongoose = require('mongoose')
-
+const { PORT, CLIENT_ORIGIN, DATABASE_URL } = require('./config');
+const { dbConnect } = require('./db-mongoose');
+const mongoose = require('mongoose');
 
 // const {dbConnect} = require('./db-knex');
 
-const { router: usersRouter} = require('./users');
+const { router: usersRouter } = require('./users');
 const { User } = require('./users/models');
-
 
 const app = express();
 
@@ -21,50 +21,51 @@ app.use(bodyParser.json());
 app.use('/api/users/', usersRouter);
 
 app.use(
-    morgan(process.env.NODE_ENV === 'production' ? 'common' : 'dev', {
-        skip: (req, res) => process.env.NODE_ENV === 'test'
-    })
+  morgan(process.env.NODE_ENV === 'production' ? 'common' : 'dev', {
+    skip: (req, res) => process.env.NODE_ENV === 'test'
+  })
 );
 
 app.use(
-    cors({
-        origin: CLIENT_ORIGIN,
-    })
+  cors({
+    origin: CLIENT_ORIGIN
+  })
 );
 
-app.get('/test', (req, res) => { 
-    return User.find().then(users => res.json(users.map(user => user.apiRepr()))) 
-    .catch(err => res.status(500).json({message: 'Internal server error'})); });
+app.get('/test', (req, res) => {
+  return User.find()
+    .then(users => res.json(users.map(user => user.apiRepr())))
+    .catch(err => res.status(500).json({ message: 'Internal server error' }));
+});
 
 function runServer(port = PORT) {
-    const server = app
-        .listen(port, () => {
-            console.info(`App listening on port ${server.address().port}`);
-        })
-        .on('error', err => {
-            console.error('Express failed to start');
-            console.error(err);
-        });
+  const server = app
+    .listen(port, () => {
+      console.info(`App listening on port ${server.address().port}`);
+    })
+    .on('error', err => {
+      console.error('Express failed to start');
+      console.error(err);
+    });
 }
 
 function closeServer() {
-    return mongoose.disconnect().then(() => {
-       return new Promise((resolve, reject) => {
-         console.log('Closing server');
-         server.close(err => {
-             if (err) {
-                 return reject(err);
-             }
-             resolve();
-         });
-       });
+  return mongoose.disconnect().then(() => {
+    return new Promise((resolve, reject) => {
+      console.log('Closing server');
+      server.close(err => {
+        if (err) {
+          return reject(err);
+        }
+        resolve();
+      });
     });
-  }
-
-
-if (require.main === module) {
-    dbConnect();
-    runServer();
+  });
 }
 
-module.exports = {app, runServer, closeServer};
+if (require.main === module) {
+  dbConnect();
+  runServer();
+}
+
+module.exports = { app, runServer, closeServer };
