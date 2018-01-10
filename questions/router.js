72d86df6ac0d9fecf5c542ca2f5ff6a7214d2c9e
Adam Bradbury <f.adambrad@gmail.com>
2018-01-10 16:48:25 -0700

    added findOne to get questions router

--- questions/router.js
+++ questions/router.js
@@ -3,19 +3,38 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const passport = require('passport');
 const { Question } = require('./models');
+const { User } = require('../users/models');
 
 const router = express.Router();
 
 const jsonParser = bodyParser.json();
 
+const jwtAuth = passport.authenticate('jwt', { session: false });
+
 // add JWT tokent to params
 // /nextquestion endpoint
+
 router.get('/', (req, res) => {
-  // change to use User.findOne() collection
-  return Question.find().then(question => res.json(question.map(question => question.apiRepr()))) 
-  // user.question[user.head]
-    .catch(err => res.status(500).json({message: 'Internal server error'})); });
+// console.log(req.user, '+++++++++++++++++++++++++++++');
+  User.findOne({username: req.users.answer})
+    .then(user => {
+      res.json(user.apiRepr());
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ message: 'Internal server error' });
+    });
+});
+
+// router.get('/', (req, res) => {
+//   // change to use User.findOne() collection
+//   return Question.find().then(question => res.json(question.map(question => question.apiRepr()))) 
+//   // user.question[user.head]
+//     .catch(err => res.status(500).json({message: 'Internal server error'})); });
+
+
 
 
     //router.post('/userAnswer'){
